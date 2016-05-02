98ccb4a21eb65cce8bb5a56fc0a3d738bbe6105e
Matthew Corner <mcorner@signiant.com>
2016-05-02 15:28:11 -0400

    Modified script to look for verify file in the scripts directory, made sure to fail.

--- execute_scripts.sh
+++ execute_scripts.sh
@@ -3,6 +3,7 @@
 HELPTEXT="Usage: execute_scripts.sh <public_key> <path_to_scripts>"
 PUBLIC_KEY=$1
 FILE_TO_EXECUTE=$2
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ -z "$1" ]; then
     echo $HELPTEXT
@@ -25,7 +26,7 @@ do
       break
   fi
 
-  ./verify_file.sh $1 $f 2>&1 | logger
+  $SCRIPT_DIR/verify_file.sh $1 $f
   if [ "$?" != 0 ]; then
       logger "ATC-$$: ERROR: Could not verify $f -- skipping execution"
   else
